2389a96eaa513d3b503993a84a002fa2bd4562b1
582123379@qq.com <582123379@qq.com>
2018-06-13 17:32:16 +0800

    dubbo

--- distribute-dubbo/dubbo-single-consumer/src/main/java/com/wolfman/dubbo/consumer/DubboSingleConsumer.java
+++ distribute-dubbo/dubbo-single-consumer/src/main/java/com/wolfman/dubbo/consumer/DubboSingleConsumer.java
@@ -1,20 +1,35 @@
 package com.wolfman.dubbo.consumer;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.wolfman.dubbo.api.DoRequest;
 import com.wolfman.dubbo.api.DoResponse;
 import com.wolfman.dubbo.api.IHelloWorldService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class DubboSingleConsumer {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
+//        异步回掉
+//        IHelloWorldService service = (IHelloWorldService) context.getBean("orderServices");
+//        long start=System.currentTimeMillis();
+//        DoRequest request = new DoRequest();
+//        request.setName("huhao");
+//        service.sayHello(request);
+//        Future<DoResponse> response = RpcContext.getContext().getFuture();
+//        System.out.println("aaaaaaaaaa");
+//        DoResponse response1 = response.get();
+//        System.out.println(response1);
+//        long end=System.currentTimeMillis();
+//        System.out.println("总共耗时："+(end-start)/1000+"秒");
 
+        //正常流程
         IHelloWorldService service = (IHelloWorldService) context.getBean("orderServices");
-
         DoRequest request = new DoRequest();
         request.setName("huhao");
         DoResponse response = service.sayHello(request);
--- distribute-dubbo/dubbo-single-provider/src/main/java/com/wolfman/dubbo/provider/HelloWorldServiceImpl.java
+++ distribute-dubbo/dubbo-single-provider/src/main/java/com/wolfman/dubbo/provider/HelloWorldServiceImpl.java
@@ -3,7 +3,9 @@ package com.wolfman.dubbo.provider;
 import com.wolfman.dubbo.api.DoRequest;
 import com.wolfman.dubbo.api.DoResponse;
 import com.wolfman.dubbo.api.IHelloWorldService;
+import org.springframework.stereotype.Service;
 
+@Service(value = "helloWorldService")
 public class HelloWorldServiceImpl implements IHelloWorldService {
     public DoResponse sayHello(DoRequest request) {
 
