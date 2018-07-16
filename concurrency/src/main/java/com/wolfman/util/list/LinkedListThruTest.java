a06b29542d54caabe4078af587ff9fb8f5073721
582123379@qq.com <582123379@qq.com>
2018-07-16 12:35:12 +0800

    linkedlist

--- concurrency/src/main/java/com/wolfman/util/list/LinkedListThruTest.java
+++ concurrency/src/main/java/com/wolfman/util/list/LinkedListThruTest.java
@@ -1,4 +1,188 @@
 package com.wolfman.util.list;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
 public class LinkedListThruTest {
+    public static void main(String[] args) {
+
+        // 通过Iterator遍历LinkedList
+        iteratorLinkedListThruIterator(getLinkedList()) ;
+
+        // 通过快速随机访问遍历LinkedList
+        iteratorLinkedListThruForeach(getLinkedList()) ;
+
+        // 通过for循环的变种来访问遍历LinkedList
+        iteratorThroughFor2(getLinkedList()) ;
+
+        // 通过PollFirst()遍历LinkedList
+        iteratorThroughPollFirst(getLinkedList()) ;
+
+        // 通过PollLast()遍历LinkedList
+        iteratorThroughPollLast(getLinkedList()) ;
+
+        // 通过removeFirst()遍历LinkedList
+        iteratorThroughRemoveFirst(getLinkedList()) ;
+
+        // 通过removeLast()遍历LinkedList
+        iteratorThroughRemoveLast(getLinkedList()) ;
+    }
+
+    private static LinkedList getLinkedList() {
+        LinkedList llist = new LinkedList();
+        for (int i=0; i<100; i++)
+            llist.addLast(i);
+
+        return llist;
+    }
+    /**
+     * 通过快迭代器遍历LinkedList
+     */
+    private static void iteratorLinkedListThruIterator(LinkedList<Integer> list) {
+        if (list == null)
+            return ;
+
+        // 记录开始时间
+        long start = System.currentTimeMillis();
+        System.out.print("iteratorLinkedListThruIterator：");
+        for(Iterator iter = list.iterator(); iter.hasNext();){
+            System.out.print(iter.next() + ", ");
+        }
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        long interval = end - start;
+        System.out.println();
+        System.out.println("iteratorLinkedListThruIterator：" + interval+" ms");
+    }
+
+    /**
+     * 通过快速随机访问遍历LinkedList
+     */
+    private static void iteratorLinkedListThruForeach(LinkedList<Integer> list) {
+        if (list == null)
+            return ;
+
+        // 记录开始时间
+        long start = System.currentTimeMillis();
+
+        int size = list.size();
+        System.out.print("iteratorLinkedListThruForeach：");
+        for (int i=0; i<size; i++) {
+            System.out.print(list.get(i) + ", ");
+        }
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        long interval = end - start;
+        System.out.println();
+        System.out.println("iteratorLinkedListThruForeach：" + interval+" ms");
+    }
+
+    /**
+     * 通过另外一种for循环来遍历LinkedList
+     */
+    private static void iteratorThroughFor2(LinkedList<Integer> list) {
+        if (list == null)
+            return ;
+
+        // 记录开始时间
+        long start = System.currentTimeMillis();
+
+        System.out.print("iteratorThroughFor2：");
+        for (Integer integ:list){
+            System.out.print(integ + ", ");
+        }
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        long interval = end - start;
+        System.out.println();
+        System.out.println("iteratorThroughFor2：" + interval+" ms");
+    }
+
+    /**
+     * 通过pollFirst()来遍历LinkedList
+     */
+    private static void iteratorThroughPollFirst(LinkedList<Integer> list) {
+        if (list == null)
+            return ;
+
+        // 记录开始时间
+        long start = System.currentTimeMillis();
+        System.out.print("iteratorThroughPollFirst：");
+        while(list.size()>0){
+            System.out.print(list.pollFirst() + ", ");
+        }
+        System.out.println();
+        System.out.println(list.size());
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        long interval = end - start;
+        System.out.println("iteratorThroughPollFirst：" + interval+" ms");
+    }
+
+    /**
+     * 通过pollLast()来遍历LinkedList
+     */
+    private static void iteratorThroughPollLast(LinkedList<Integer> list) {
+        if (list == null)
+            return ;
+
+        // 记录开始时间
+        long start = System.currentTimeMillis();
+        System.out.print("iteratorThroughPollLast：");
+        while(list.size()>0){
+            System.out.print(list.pollLast() + ", ");
+        }
+        System.out.println();
+        System.out.println(list.size());
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        long interval = end - start;
+        System.out.println("iteratorThroughPollLast：" + interval+" ms");
+    }
+
+    /**
+     * 通过removeFirst()来遍历LinkedList
+     */
+    private static void iteratorThroughRemoveFirst(LinkedList<Integer> list) {
+        if (list == null)
+            return ;
+
+        // 记录开始时间
+        long start = System.currentTimeMillis();
+
+
+        System.out.print("iteratorThroughRemoveFirst：");
+        while(list.size()>0){
+            System.out.print(list.removeFirst() + ", ");
+        }
+        System.out.println();
+        System.out.println(list.size());
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        long interval = end - start;
+        System.out.println("iteratorThroughRemoveFirst：" + interval+" ms");
+    }
+
+    /**
+     * 通过removeLast()来遍历LinkedList
+     */
+    private static void iteratorThroughRemoveLast(LinkedList<Integer> list) {
+        if (list == null)
+            return ;
+
+        // 记录开始时间
+        long start = System.currentTimeMillis();
+        System.out.print("iteratorThroughRemoveLast：");
+        while(list.size()>0){
+            System.out.print(list.removeLast() + ", ");
+        }
+        System.out.println();
+        System.out.println(list.size());
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        long interval = end - start;
+        System.out.println("iteratorThroughRemoveLast：" + interval+" ms");
+    }
+
 }
