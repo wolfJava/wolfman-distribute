6c98c7cbf18b5b8d376d01ecfeae40e9da81d324
582123379@qq.com <582123379@qq.com>
2018-08-13 17:59:56 +0800

    CharArrayReader

--- concurrency/src/main/java/com/wolfman/io/file/FileDemo.java
+++ concurrency/src/main/java/com/wolfman/io/file/FileDemo.java
@@ -10,6 +10,9 @@ public class FileDemo {
 
     public static void main(String[] args) throws URISyntaxException {
 
+        System.out.println();
+
+
         try {
             File dir = new File("dir");    // 获取目录“dir”对应的File对象
             File file1 = new File(dir, "file1.txt");
